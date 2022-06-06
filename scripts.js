63776a6f97e3aef7d539ded016cc9fa0487a1bbf
Bella Novicki <bella.novicki@gmail.com>
2022-06-06 14:35:37 -0500

    styling edits

--- scripts.js
+++ scripts.js
@@ -308,8 +308,8 @@ function newQuote() {
   const plusBtn = document.getElementById("plusBtn");
   const minusBtn = document.getElementById("minusBtn");
 
-  plusBtn.addEventListener("click", increment());
-  minusBtn.addEventListener("click", decrement());
+  plusBtn.addEventListener("touchstart", increment);
+  minusBtn.addEventListener("click", decrement);
 
   document.getElementById("sourceDisplay").innerHTML = quoteSource; //displays the source of the quote
   document.getElementById("quoteDisplay").innerHTML = finalQuote; //displays the final prompt
